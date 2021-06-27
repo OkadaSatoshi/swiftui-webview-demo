f3becbf990ae0348f178e0d40cb7a31dcd3bf892
SatoshiOkada <soysoysoup@gmail.com>
2021-06-27 14:11:19 +0900

    コードの整理

--- swiftui-webview-demo/ContentView.swift
+++ swiftui-webview-demo/ContentView.swift
@@ -9,43 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     
-//    @ObservedObject var stateModel = WebViewStateModel(url: "https://apple.com")
-    
     var body: some View {
         NavigationView {
             ZStack {
                 MyWebView(url: "https://www.apple.com/")
-//                if stateModel.isLoading {
-//                    ProgressView()
-//                }
             }
             .navigationBarTitle(Text("--------"), displayMode: .inline)
-            .navigationBarItems(leading: Button(action: {
-                
-            }, label: {
-                Text("back")
-            }), trailing: Button(action: {
-                
-            }, label: {
-                Text("back")
-            }))
-//                leading: Button(action: {
-////                    stateModel.shouldGoBack = true
-//                    print("--------back----")
-//                }) {
-//                    if stateModel.canGoBack {
-//                        Text("<Back")
-//                    } else {
-//                        EmptyView()
-//                    }
-//                },
-//                trailing: Button(action: {
-//                    print("=======google====")
-////                    stateModel.load("https://google.com")
-//                }) {
-//                    Text("Google")
-//                }
-//            )
         }
     }
 }
