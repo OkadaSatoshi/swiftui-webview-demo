49be60d4b2e39d4d1bd21dd78f5cefcfdad5d2c2
SatoshiOkada <soysoysoup@gmail.com>
2021-06-27 13:42:38 +0900

    Initial Commit

---
+++ swiftui-webview-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-webview-demo
+//
+//  Created by 岡田聡 on 2021/06/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
